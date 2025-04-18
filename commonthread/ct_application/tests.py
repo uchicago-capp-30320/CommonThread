edb329f7b173dc5526f347920c8c223efe8ae7ec
paul soltys <pjsol79@gmail.com>
2025-04-18 17:20:19 -0500

    Test is running locally- test is failing because I am not pulling the url properly

--- commonthread/ct_application/tests.py
+++ commonthread/ct_application/tests.py
@@ -1,6 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-import backend.tests as bt
-import frontend.tests as ft
+#import backend.tests as bt
+#import frontend.tests as ft
 
 # Create your tests here.
+
+# Sample Test: To be moved to different folder on proof of concept
+class PageViewTests(TestCase):
+    '''
+    Class for testing status codes & outputs for templates/views for the frontend
+    '''
+    def homepage_test(self):
+        response = self.client.get(reverse('/'))
+        assert response.status_code == 200
+
+sample_test = PageViewTests()
+sample_test.homepage_test()
