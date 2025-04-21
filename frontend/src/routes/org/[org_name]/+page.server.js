dd820e2e130f51c6965d4df4db2b7282e8148574
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2025-04-21 12:05:16 -0500

    sample routing

---
+++ frontend/src/routes/org/[org_name]/+page.server.js
@@ -0,0 +1,14 @@
+export async function load() {
+	const tests = [
+		{
+			name: 'Test',
+			email: 'test@gmail.com'
+		},
+		{
+			name: 'Test2',
+			email: 'test2@gmail.com'
+		}
+	];
+
+	return { tests };
+}
--- frontend/src/routes/org/[org_name]/org-admin/+page.svelte
+++ frontend/src/routes/org/[org_name]/org-admin/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Landing Page</h1>
+<h1>Org Admin Name</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
