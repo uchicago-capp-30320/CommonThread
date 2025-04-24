f933eaee24ac56514813cea7022043baf6f09b63
paul soltys <pjsol79@gmail.com>
2025-04-24 16:39:08 -0500

    tests.py is now empty- can likely be deleted in favor of test folder

--- commonthread/ct_application/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, SimpleTestCase
-
-from ct_application.backend.tests import *
-# Create your tests here.
-
-# Sample Test: To be moved to different folder on proof of concept
-
-
-
