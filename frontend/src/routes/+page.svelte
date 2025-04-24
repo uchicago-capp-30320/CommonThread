f90d911d53154e2f1cc16bc8696b2185694e08fc
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2025-04-23 19:16:36 -0500

    add bulma

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Landing Page</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class = "is-size-1">Landing Page</h1>
+<p class = 'box'>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
