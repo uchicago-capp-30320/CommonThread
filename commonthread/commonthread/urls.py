8f5bb1839d26c6c6128befb5383e66e3ba06326a
pravchand <praveenc@uchicago.edu>
2025-04-29 17:35:55 -0500

    add get-story

--- commonthread/commonthread/urls.py
+++ commonthread/commonthread/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",home_test, name="home_test"),
     path("stories/",get_story, name="get_stories"),
+    path("stories/<int:story_id/",get_story, name="get_story"),
     path("org/<int:org_id>/<int:user_id>/", show_org_dashboard, name="show_org_dashboard"),
     path("org/<int:org_id>/<int:user_id>/project/<int:project_id>/", show_project_dashboard, name="show_project_dashboard"),
 ]
