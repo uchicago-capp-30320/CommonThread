bb31154371e7dd9390ae840ed53a3b9b34b3df71
pravchand <praveenc@uchicago.edu>
2025-05-05 16:33:56 -0500

    utils for generating tokens

---
+++ commonthread/ct_application/utils.py
@@ -0,0 +1,19 @@
+from commonthread.settings import JWT_SECRET_KEY,JWT_REFRESH_SECRET_KEY
+import datetime
+import jwt
+
+def generate_access_token(user_id):
+    payload = {
+        'sub': user_id,
+        'exp': datetime.datetime.now() + datetime.timedelta(hours = 2),
+        'iat': datetime.datetime.now()
+    }
+    return jwt.encode(payload,JWT_SECRET_KEY,algorithm = 'HS256')
+
+def generate_refresh_token(user_id):
+    payload = {
+        'sub': user_id,
+        'exp': datetime.datetime.now() + datetime.timedelta(days=7),
+        'iat': datetime.datetime.now()
+    }
+    return jwt.encode(payload,JWT_REFRESH_SECRET_KEY,algoritm ='HS256')
