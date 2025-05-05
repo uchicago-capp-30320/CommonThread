a06fedf88b4581ef82915424e5720b74cf6cfccb
pravchand <praveenc@uchicago.edu>
2025-05-04 19:58:12 -0500

    add improperly configured

--- commonthread/commonthread/settings.py
+++ commonthread/commonthread/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
