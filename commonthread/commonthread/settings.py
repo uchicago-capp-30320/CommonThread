565ad6d1024109ed5be196b854c2f539860b3d14
pravchand <praveenc@uchicago.edu>
2025-05-05 16:33:03 -0500

    add secrets in settings JWT

--- commonthread/commonthread/settings.py
+++ commonthread/commonthread/settings.py
@@ -31,6 +31,8 @@ DATABASE_USER = os.getenv("DATABASE_USER")
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_PORT = os.getenv("DATABASE_PORT")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_REFRESH_SECRET_KEY = os.getenv("JWT_REFRESH_SECRET_KEY")
 
 # if not SECRET_KEY:
     # raise ImproperlyConfigured("Secret Key not set in .env! Please set SECRET_KEY in an .env file next to your manage.py")
