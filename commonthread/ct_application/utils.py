e003e7699f9a19941efee344e8f5cb9d6f37a586
Onurcan Buyukkalkan <onurcan@uchicago.edu>
2025-05-06 17:20:31 -0500

    Typos fixed
    Algoritm -> algorithm

--- commonthread/ct_application/utils.py
+++ commonthread/ct_application/utils.py
@@ -16,7 +16,7 @@ def generate_refresh_token(user_id):
         'exp': datetime.datetime.now() + datetime.timedelta(days=7),
         'iat': datetime.datetime.now()
     }
-    return jwt.encode(payload,JWT_REFRESH_SECRET_KEY,algoritm ='HS256')
+    return jwt.encode(payload,JWT_REFRESH_SECRET_KEY,algorithm ='HS256')
 
 def decode_refresh_token(token):
-    return jwt.decode(token,JWT_REFRESH_SECRET_KEY,algoritm ='HS256')
+    return jwt.decode(token,JWT_REFRESH_SECRET_KEY,algorithm ='HS256')
