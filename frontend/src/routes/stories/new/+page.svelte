bc90c30165a305bd9f816919662e868416dfd757
pravchand <praveenc@uchicago.edu>
2025-05-06 14:08:43 -0500

    ADD story route

---
+++ frontend/src/routes/stories/new/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import StoryInput from '../../../components/story/StoryHolder.svelte';
+	import '/src/app.css';
+</script>
+
+<div class="page-container is-flex is-flex-direction-column is-align-items-center p-5 is-fullwidth">
+	<StoryInput />
+</div>
+
+<style>
+	.page-container {
+		background-image: url('$lib/assets/background_texture.png');
+		min-height: 100vh; /* so that container takes up the full height */
+	}
+</style>
