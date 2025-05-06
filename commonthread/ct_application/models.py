81a14cbcf78b247f1ae14896100d65533d745607
Regina I. Medina <regimedina19@gmail.com>
2025-05-05 23:27:27 -0500

    Updated and added tests for login, create_user and add_user_to_org endpoints

--- commonthread/ct_application/models.py
+++ commonthread/ct_application/models.py
@@ -1,70 +1,87 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-#consider use of uniqueconstraints
+# consider use of uniqueconstraints
 
 ################################### User/Auth Tables ##########################################
 
+
 class CustomUser(AbstractUser):
-    user_id = models.AutoField(primary_key = True)
-    name = models.CharField("display name", max_length = 30)
-    
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField("display name", max_length=30)
+
+
 # user-login
 class UserLogin(models.Model):
-    user_id = models.OneToOneField(CustomUser, primary_key = True, on_delete = models.CASCADE)
-    username = models.CharField(max_length = 255, unique = True)
-    password = models.CharField(max_length = 255) # This probably changes based on PW storage method
+    user_id = models.OneToOneField(
+        CustomUser, primary_key=True, on_delete=models.CASCADE
+    )
+    username = models.CharField(max_length=255, unique=True)
+    password = models.CharField(
+        max_length=255
+    )  # This probably changes based on PW storage method
+
 
 ###################################### Story Tables ##########################################
+
 
 # organization
 class Organization(models.Model):
     org_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    
+
+
 # project
 class Project(models.Model):
     proj_id = models.AutoField(primary_key=True)
-    org_id = models.ForeignKey(Organization, on_delete = models.CASCADE)
-    name = models.CharField(max_length=100) 
-    curator = models.ForeignKey(CustomUser, models.SET_NULL, blank = True, null = True)
+    org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    curator = models.ForeignKey(CustomUser, models.SET_NULL, blank=True, null=True)
     date = models.DateField()
+
 
 # story
 class Story(models.Model):
     story_id = models.AutoField(primary_key=True)
-    proj_id = models.ForeignKey(Project, on_delete = models.CASCADE)
-    org_id = models.ForeignKey(Organization, on_delete = models.CASCADE)
-    storyteller = models.CharField(max_length = 100)
-    curator = models.ForeignKey(CustomUser, models.SET_NULL, blank = True, null = True) #Just null curator if user is deleted
+    proj_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    storyteller = models.CharField(max_length=100)
+    curator = models.ForeignKey(
+        CustomUser, models.SET_NULL, blank=True, null=True
+    )  # Just null curator if user is deleted
     date = models.DateField()
     content = models.TextField()
 
+
 ####################################### TAG TABLES #######################################
+
 
 # tag
 class Tag(models.Model):
-    tag_id = models.AutoField(primary_key = True)
-    name = models.CharField(max_length = 100)
+    tag_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
 
 # story-tag
 class StoryTag(models.Model):
-    story_tag_id = models.AutoField(primary_key = True)
-    story_id = models.ForeignKey(Story, on_delete = models.CASCADE)
-    tag_id = models.ForeignKey(Tag, on_delete = models.CASCADE)
+    story_tag_id = models.AutoField(primary_key=True)
+    story_id = models.ForeignKey(Story, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
 
 # project-tag
 class ProjectTag(models.Model):
-    proj_tag_id = models.AutoField(primary_key = True)
-    proj_id = models.ForeignKey(Project, on_delete = models.CASCADE)
-    tag_id = models.ForeignKey(Tag, on_delete = models.CASCADE)
+    proj_tag_id = models.AutoField(primary_key=True)
+    proj_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
 
 ################################# ADMIN TABLES ############################################
 
+
 # org-user
 class OrgUser(models.Model):
-    org_user_id = models.AutoField(primary_key = True)
-    user_id = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
-    org_id = models.ForeignKey(Organization, on_delete = models.CASCADE)
-    access = models.CharField(max_length = 20)
+    org_user_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    access = models.CharField(max_length=20)
--- commonthread/ct_application/views.py
+++ commonthread/ct_application/views.py
@@ -3,9 +3,15 @@ from datetime import date
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_GET, require_POST, require_http_methods
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
-from .utils import generate_access_token, generate_refresh_token,decode_refresh_token
-from django.contrib.auth import get_user_model
+from django.http import (
+    HttpResponse,
+    JsonResponse,
+    HttpResponseNotFound,
+    HttpResponseForbidden,
+    HttpResponseBadRequest,
+)
+from .utils import generate_access_token, generate_refresh_token, decode_refresh_token
+from django.contrib.auth import authenticate, get_user_model
 from .models import Organization, OrgUser, Project, Story, Tag, ProjectTag, UserLogin
 import jwt
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
@@ -15,47 +21,49 @@ User = get_user_model()
 
 
 # Create your views here.
-@ensure_csrf_cookie # Need this for POSTMAN testing purposes. Otherwise
+@ensure_csrf_cookie  # Need this for POSTMAN testing purposes. Otherwise
 # CSRF token is not received in a single GET and POST requests fail.
 def home_test(request):
     return HttpResponse("Welcome to the Common Threads Home Page!", status=200)
 
-def login(request): 
+
+def login(request, username, password):
     """
     TODO: Incorporate JWT
     """
-    authorized = True
-    if authorized: 
+    authenticated_user = authenticate(username=username, password=password)
+
+    if authenticated_user is not None:
+        # A backend authenticated the credentials
         return HttpResponse("Login successful", status=200)
-    return HttpResponse("Login usuccesfful", status=403)
+    else:
+        # No backend authenticated the credentials
+        return HttpResponse("Login usuccesfful", status=403)
 
 
 def show_project_dashboard(request, user_id, org_id, project_id):
     # check user org and project IDs are provided
     if not all([user_id, org_id, project_id]):
         return HttpResponseNotFound(
-            "User ID, Organization ID, or Project ID not provided.",
-            status=404
+            "User ID, Organization ID, or Project ID not provided.", status=404
         )
     # load user and org or throw 404 if not found
-    user = get_object_or_404(User,   pk=user_id)
-    org  = get_object_or_404(Organization, pk=org_id)
+    user = get_object_or_404(User, pk=user_id)
+    org = get_object_or_404(Organization, pk=org_id)
 
     # check if user is indeed a member of the org
     try:
         _ = OrgUser.objects.get(user_id=user, org_id=org)
     except OrgUser.DoesNotExist:
         return HttpResponseForbidden(
-            "You are not a member of this organization! Not authorized.",
-            status=403
+            "You are not a member of this organization! Not authorized.", status=403
         )
 
     # project may not belong to the org, so check that too
     project = get_object_or_404(Project, pk=project_id)
     if project.org_id != org:
         return HttpResponseNotFound(
-            "Project does not belong to this organization. Not authorized.",
-            status=404
+            "Project does not belong to this organization. Not authorized.", status=404
         )
 
     # how will this access level be defined? admin, curator, member?
@@ -70,38 +78,40 @@ def show_project_dashboard(request, user_id, org_id, project_id):
 
     # load whatever data you need for the dashboard and send to frontend
     story_count = Story.objects.filter(proj_id=project).count()
-    tag_count   = ProjectTag.objects.filter(proj_id=project).count()
+    tag_count = ProjectTag.objects.filter(proj_id=project).count()
     # data= {
     #     "user": user,
     #     "org": org,
     #     "project": project,
     #     # Add any other data you need for the dashboard
-    # }   
+    # }
 
-    return JsonResponse({
-        "project_id":  project.proj_id,
-        "story_count": story_count,
-        "tag_count":   tag_count,
-    }, status=200)
+    return JsonResponse(
+        {
+            "project_id": project.proj_id,
+            "story_count": story_count,
+            "tag_count": tag_count,
+        },
+        status=200,
+    )
+
 
 def show_org_dashboard(request, user_id, org_id):
     # check user org and project IDs are provided
     if not all([user_id, org_id]):
         return HttpResponseNotFound(
-            "User ID or Organization ID not provided.",
-            status=404
+            "User ID or Organization ID not provided.", status=404
         )
     # load user and org or throw 404 if not found
-    user = get_object_or_404(User,   pk=user_id)
-    org  = get_object_or_404(Organization, pk=org_id)
+    user = get_object_or_404(User, pk=user_id)
+    org = get_object_or_404(Organization, pk=org_id)
 
     # check if user is indeed a member of the org
     try:
         _ = OrgUser.objects.get(user_id=user, org_id=org)
     except OrgUser.DoesNotExist:
         return HttpResponseForbidden(
-            "You are not a member of this organization! Not authorized.",
-            status=403
+            "You are not a member of this organization! Not authorized.", status=403
         )
 
     # get all projects for the organization
@@ -112,183 +122,211 @@ def show_org_dashboard(request, user_id, org_id):
     #     "user": user,
     #     "org": org,
     #     "projects": projects,
-    # }   
+    # }
 
-    return JsonResponse({
-        "organization_id": org.org_id,
-        "organization_name": org.name,
-        "project_count": project_count,
-    }, status=200)
+    return JsonResponse(
+        {
+            "organization_id": org.org_id,
+            "organization_name": org.name,
+            "project_count": project_count,
+        },
+        status=200,
+    )
+
 
 ###############################################################################
 @require_GET
-#TODO authentication and authorization check
-def get_story(request,story_id = None):
+# TODO authentication and authorization check
+def get_story(request, story_id=None):
     if story_id:
         try:
-            story = Story.objects.get(story_id = story_id)
-            return JsonResponse({
-                "story_id": story.story_id,
-                "storyteller": story.storyteller,
-                "curator":story.curator.user_id if story.curator else None,
-                "date": story.date,
-                "content": story.content
-            }, status = 200)
+            story = Story.objects.get(story_id=story_id)
+            return JsonResponse(
+                {
+                    "story_id": story.story_id,
+                    "storyteller": story.storyteller,
+                    "curator": story.curator.user_id if story.curator else None,
+                    "date": story.date,
+                    "content": story.content,
+                },
+                status=200,
+            )
         except Story.DoesNotExist:
             return HttpResponseNotFound(
-            "Could not find that story. It has been either deleted or misplaced",
-            status=404
-        )
+                "Could not find that story. It has been either deleted or misplaced",
+                status=404,
+            )
     else:
         try:
-            #TODO Decide if all the details are needed if all stories are asked for
+            # TODO Decide if all the details are needed if all stories are asked for
             stories = Story.objects.all()
-            return JsonResponse({
-                'stories': list(stories.values("story_id","storyteller","curator","date","content"))
-            },status = 200)
+            return JsonResponse(
+                {
+                    "stories": list(
+                        stories.values(
+                            "story_id", "storyteller", "curator", "date", "content"
+                        )
+                    )
+                },
+                status=200,
+            )
         except Exception as e:
-            return JsonResponse({"success": False, "error":str(e)}, status = 500)
+            return JsonResponse({"success": False, "error": str(e)}, status=500)
 
 
 ###############################################################################
+
 
 @require_POST
 def create_user(request):
-    """ 
-    Receives a request with user_id, username and password values in its body 
-    and registers new user in the login table of the db. 
-    # TODO: Check that user is does not already exists 
-    # TODO: Send email confirmation 
-    # TODO: Update docstrings and specify exceptions 
+    """
+    Receives a request with user_id, username and password values in its body
+    and registers new user in the login table of the db.
+    # TODO: Check that user is does not already exists
+    # TODO: Send email confirmation
+    # TODO: Update docstrings and specify exceptions
+
+    Ref: https://docs.djangoproject.com/en/5.2/topics/auth/default/
     """
     try:
-        user_data = json.loads(request.body or '{}')
+        user_data = json.loads(request.body or "{}")
     except json.JSONDecodeError:
         return JsonResponse({"success": False, "error": "Invalid JSON"}, status=400)
-    
-    # Check that all required keys are in the request 
-    required_keys = ["user_id", "username", "password"]
+
+    # Check that all required keys are in the request
+    required_keys = ["username", "password"]
     missing_keys = [key for key in required_keys if key not in user_data]
 
-    if missing_keys: 
+    if missing_keys:
         return JsonResponse(
-            {"success": False, 
-             "error":f"The following required keys are missing: {missing_keys}"})
-
-    # Update Django model  
-    try: 
-        UserLogin.objects.create(
-            user_id = user_data["user_id"], 
-            username = user_data["username"], 
-            password = user_data["password"]
+            {
+                "success": False,
+                "error": f"The following required keys are missing: {missing_keys}",
+            }
         )
-    except Exception as e: 
-        return JsonResponse({
-            "success": False,
-            "error": str(e)
-        }, status=400)
 
+    try:
+        user = UserLogin.objects.create_user(
+            username=user_data["username"],
+            password=user_data["password"],
+        )
+    except Exception as e:
+        return JsonResponse({"success": False, "error": str(e)}, status=400)
+
+    # Add optional fields if provided
+    if "name" in user_data:
+        user.name = user_data["name"]
+    if "email" in user_data:
+        user.email = user_data["email"]
+    if "last_name" in user_data:
+        user.last_name = user_data["last_name"]
+    if "first_name" in user_data:
+        user.first_name = user_data["first_name"]
+
+    user.save()
+
+    return JsonResponse(
+        {
+            "success": True,
+        },
+        status=201,
+    )
 
 
 ###############################################################################
+
 
 @require_POST
 def add_user_to_org(request):
-    """ 
-    Receives a request with user_id and org_id its body and registers new user 
-    user-org relationship in the login table of the db. 
-    # TODO: Update docstrings and specify exceptions 
+    """
+    Receives a request with user_id and org_id its body and registers new user
+    user-org relationship in the login table of the db.
+    # TODO: Update docstrings and specify exceptions
     """
     try:
-        org_user_data = json.loads(request.body or '{}')
+        org_user_data = json.loads(request.body or "{}")
     except json.JSONDecodeError:
         return JsonResponse({"success": False, "error": "Invalid JSON"}, status=400)
-    
-    # Check that all required keys are in the request 
+
+    # Check that all required keys are in the request
     required_keys = ["user_id", "org_id"]
     missing_keys = [key for key in required_keys if key not in org_user_data]
 
-    if missing_keys: 
+    if missing_keys:
         return JsonResponse(
-            {"success": False, 
-             "error":f"The following required keys are missing: {missing_keys}"})
-
-    if "access" in org_user_data: 
-        access = org_user_data["access"]
-    else: 
-        access = None
-    try: 
-        OrgUser.objects.create(
-            user_id = org_user_data["user_id"], 
-            org_id = org_user_data["org_id"],
-            access = org_user_data["access"]
+            {
+                "success": False,
+                "error": f"The following required keys are missing: {missing_keys}",
+            }
         )
-    except Exception as e: 
-        return JsonResponse({
-            "success": False,
-            "error": str(e)
-        }, status=400)
+
+    # TODO: Get organization auth
+    try:
+        OrgUser.objects.create(
+            user_id=org_user_data["user_id"],
+            org_id=org_user_data["org_id"],
+            access=org_user_data["access"],
+        )
+    except Exception as e:
+        return JsonResponse({"success": False, "error": str(e)}, status=400)
 
 
 ###############################################################################
 @require_POST
-#TODO authentication and authorization check
+# TODO authentication and authorization check
 def create_story(request):
     try:
-        #TODO connect story to a project
+        # TODO connect story to a project
         story_data = json.loads(request.body)
         _ = Story.objects.create(
-            story_id    = story_data["story_id"],
-            storyteller = story_data["storyteller"],
-            curator_id  = story_data["curator"],
-            date        = story_data["date"],
-            content     = story_data["content"],
-            proj_id_id  = story_data["proj_id"],
-            org_id_id   = story_data["org_id"],
+            story_id=story_data["story_id"],
+            storyteller=story_data["storyteller"],
+            curator_id=story_data["curator"],
+            date=story_data["date"],
+            content=story_data["content"],
+            proj_id_id=story_data["proj_id"],
+            org_id_id=story_data["org_id"],
         )
-        return JsonResponse({
-            "story_id": story_data.get("story_id")
-        }, status = 200)
-    
+        return JsonResponse({"story_id": story_data.get("story_id")}, status=200)
+
     except Exception as e:
-        return JsonResponse({
-            "success": False,
-            "error": str(e)
-        }, status=400)
-    
+        return JsonResponse({"success": False, "error": str(e)}, status=400)
+
+
 ###############################################################################
 @require_POST
-#TODO authentication and authorization check
+# TODO authentication and authorization check
 def create_project(request):
 
     try:
-        project_data = json.loads(request.body or '{}')
+        project_data = json.loads(request.body or "{}")
     except json.JSONDecodeError:
         return JsonResponse({"success": False, "error": "Invalid JSON"}, status=400)
 
-    org_id = project_data.get('org_id')
+    org_id = project_data.get("org_id")
     if not org_id:
-        return JsonResponse({
-            "success": False,
-            "error": 'Organization is required'
-        }, status=400)
-    
-    #Need to get org as object to pass to the project creation
+        return JsonResponse(
+            {"success": False, "error": "Organization is required"}, status=400
+        )
+
+    # Need to get org as object to pass to the project creation
     try:
         org = Organization.objects.get(pk=org_id)
     except Organization.DoesNotExist:
-        return JsonResponse({"success": False, "error": "Organization not found"}, status=404)
-    
+        return JsonResponse(
+            {"success": False, "error": "Organization not found"}, status=404
+        )
+
     try:
         project = Project.objects.create(
-            name=project_data['name'],
-            curator_id=project_data['curator'],
+            name=project_data["name"],
+            curator_id=project_data["curator"],
             org_id=org,
-            date=project_data.get('date', str(date.today()))
+            date=project_data.get("date", str(date.today())),
         )
 
         # move the tag loop inside the try
-        tags = project_data.get('tags', [])
+        tags = project_data.get("tags", [])
         for tag_name in tags:
             tag = Tag.objects.create(name=tag_name)
             ProjectTag.objects.create(
@@ -296,49 +334,50 @@ def create_project(request):
                 proj_id=project,
             )
 
-        return JsonResponse({
-            'success': True,
-            'project_id': project.proj_id,
-        }, status=201)
+        return JsonResponse(
+            {
+                "success": True,
+                "project_id": project.proj_id,
+            },
+            status=201,
+        )
     except KeyError as e:
         return JsonResponse({"success": False, "error": str(e)}, status=400)
     except Exception as e:
-        return JsonResponse({"success": False,
-                            "error": f"internal service error {e}"}, status=500)
+        return JsonResponse(
+            {"success": False, "error": f"internal service error {e}"}, status=500
+        )
 
 
 ###############################################################################
 
+
 @require_POST
-#TODO authentication and authorization check
+# TODO authentication and authorization check
 def create_org(request):
     org_data = json.loads(request.body)
     try:
         org = Organization.objects.create(
-            name=org_data['name'],
-            org_id=org_data['org_id'],
+            name=org_data["name"],
+            org_id=org_data["org_id"],
         )
-        user= get_user_model().objects.get(pk=org_data['user_id'])
-        OrgUser.objects.create(
-            org_id=org,
-            user_id=user,
-            access="admin"
-        )
+        user = get_user_model().objects.get(pk=org_data["user_id"])
+        OrgUser.objects.create(org_id=org, user_id=user, access="admin")
     except KeyError as e:
-        return JsonResponse({
-            "success": False,
-            "error": str(e)
-        }, status=400)
+        return JsonResponse({"success": False, "error": str(e)}, status=400)
     except Exception as e:
-        return JsonResponse({
-            "success":False,
-            "error": f"internal service error{str(e)}"
-        }, status = 500)
-    
-    return JsonResponse({
-            'success': True,
-            'org_id': org.org_id,
-        }, status=201)
+        return JsonResponse(
+            {"success": False, "error": f"internal service error{str(e)}"}, status=500
+        )
+
+    return JsonResponse(
+        {
+            "success": True,
+            "org_id": org.org_id,
+        },
+        status=201,
+    )
+
 
 ###############################################################################
 @require_GET
@@ -432,11 +471,11 @@ def show_org_admin_dashboard(request, user_id, org_id):
             )
         except json.JSONDecodeError:
             return HttpResponseBadRequest("Invalid JSON.")
-        
+
 
 @require_POST
 def get_new_access_token(request):
-    #TODO change this if they will send it in as a cookie
+    # TODO change this if they will send it in as a cookie
     data = json.loads(request.body)
     refresh_token = data.get("refresh_token")
     if not refresh_token:
@@ -452,5 +491,6 @@ def get_new_access_token(request):
     except InvalidTokenError:
         # we need to redirect to login?
         return JsonResponse({"error": "Invalid refresh token"}, status=401)
+
 
 #### EOF. ####
