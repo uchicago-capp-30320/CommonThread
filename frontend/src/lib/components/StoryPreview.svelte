6969b35213ad5a2b84fd2616da3bffef27d3fd32
Regina I. Medina <regimedina19@gmail.com>
2025-05-07 12:53:57 -0500

    Improved story preview styling

--- frontend/src/lib/components/StoryPreview.svelte
+++ frontend/src/lib/components/StoryPreview.svelte
@@ -1,67 +1,40 @@
-<script lang="ts">
+<script>
 	// Refs: https://stackoverflow.com/questions/43370534/css-cutting-text-to-the-size-of-parent-div
 	// Refs: https://www.w3schools.com/css/css3_borders.asp
 	// Refs: https://dev.to/drews256/ridiculously-easy-row-and-column-layouts-with-flexbox-1k01
-	let { name, email, text, author = '', date = '' } = $props();
+	let story = $props();
+
 </script>
 
-<div class="myStory" id="myStory">
-	<div class="row">
-		<div class="column">
-			<div class="title">
-				{name}
-			</div>
+<div class="box" id="storySample">
+	<div class="column">
+		<div class="row" style="margin-right:0;margin-left:auto<float:right">
+			<span class="tag is-info is-medium"> Storyteller: { story.storyteller } </span>
+			<span class="tag is-info is-medium"> Curator: { story.curator } </span>
+			<span class="tag is-primary is-medium" id="date"> { story.date } </span>	
 		</div>
-		<div class="column">
-			<div class="row" style="margin-right:0;margin-left:auto<float:right">
-				<button style="background-color:#56BCB3">Story by {author}</button>
-				<button style="background-color:#D0FDB9">Label 1</button>
-				<button style="background-color:white">Label 2</button>
-			</div>
+
+		<div class="text" id="storyContent">
+			{ story.content }
 		</div>
-	</div>
-	<p>{date}</p>
-	<div class="row">
-		<div class="text">
-			{text}
-		</div>
+
 	</div>
 </div>
 
-<!-- <div class="container" id="myStory">
-	"
-	<div class="title">
-		{name}
-	</div>
-	<p>{date}</p>
-	<div class="text">
-		{text}
-	</div>
-</div>
-
-<button style="background-color:#56BCB3">Story by {author}</button><button
-	style="background-color:#D0FDB9">Label 1</button
-><button style="background-color:white">Label 2</button> -->
 
 <style>
-	#myStory {
+
+	#storySample {
+		display: flex;
+		flex-direction: flex;
 		border-radius: 15px 50px 30px;
 		border: 1px solid #133335;
-		padding: 20px;
-		width: 80%;
-		height: 150px;
+		padding: 1em;
+		width: 75%;
+		height: 300px;
 		background-color: #f2f1f0;
 		filter: alpha(opacity=40);
-	}
-
-	.title {
-		font-family: 'Arial';
-		font-size: 1.5vw;
-	}
-
-	.container {
-		flex-direction: column;
-		display: flex;
+		justify-content: center;
 	}
 
 	.row {
@@ -77,12 +50,27 @@
 	.text {
 		flex: 1;
 		overflow: hidden;
-		font-family: 'Arial';
 	}
 
-	button {
-		color: #000000;
-		border: 2px solid;
-		border-radius: 25px;
+	span + span {
+		margin-left: 2%;
 	}
+
+	#date {
+		display: flex; 
+		float: right;
+	}
+
+	#storyContent {
+		margin-top: 15px;
+	}
+
+	/*
+	Alternative for overflow
+	Ref: https://css-tricks.com/almanac/properties/t/text-overflow/
+	 .ellipsis {
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		overflow: hidden;
+	} */
 </style>
