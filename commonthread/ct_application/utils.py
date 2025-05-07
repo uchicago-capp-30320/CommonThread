873d44a69401abaeead0b6c16756f2158813dd0b
Onurcan Buyukkalkan <onurcan@uchicago.edu>
2025-05-06 19:19:58 -0500

    Utils errors fixed

--- commonthread/ct_application/utils.py
+++ commonthread/ct_application/utils.py
@@ -2,21 +2,24 @@ from commonthread.settings import JWT_SECRET_KEY,JWT_REFRESH_SECRET_KEY
 import datetime
 import jwt
 
-def generate_access_token(user_id):
+def generate_access_token(user_id:int)-> str:
     payload = {
-        'sub': user_id,
+        'sub': str(user_id),
         'exp': datetime.datetime.now() + datetime.timedelta(hours = 2),
         'iat': datetime.datetime.now()
     }
     return jwt.encode(payload,JWT_SECRET_KEY,algorithm = 'HS256')
 
-def generate_refresh_token(user_id):
+def generate_refresh_token(user_id:int)-> str:
     payload = {
-        'sub': user_id,
+        'sub': str(user_id),
         'exp': datetime.datetime.now() + datetime.timedelta(days=7),
         'iat': datetime.datetime.now()
     }
     return jwt.encode(payload,JWT_REFRESH_SECRET_KEY,algorithm ='HS256')
 
 def decode_refresh_token(token):
-    return jwt.decode(token,JWT_REFRESH_SECRET_KEY,algorithms ='HS256')
+    return jwt.decode(token,JWT_REFRESH_SECRET_KEY,
+                      algorithms =['HS256'],
+                      options={"require_exp": True, "verify_exp": True}
+                      )
