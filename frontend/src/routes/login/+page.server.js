621881170b67690cbe7e9fe00ad5354cba8ce7ad
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2025-05-08 10:34:03 -0500

    log in connect

---
+++ frontend/src/routes/login/+page.server.js
@@ -0,0 +1,52 @@
+import { fail, redirect } from '@sveltejs/kit';
+
+export const load = async ({ cookies }) => {
+	// Check if the user is already logged in
+	const ct_access_token = cookies.get('ct_access_token');
+
+	console.log('ct_access_token from cookie', ct_access_token);
+	// if (ct_access_token) {
+	//     // If logged in, redirect to the dashboard or home page
+	//     throw redirect(302, '/org/org-austin');
+	// }
+
+	return {};
+};
+
+export const actions = {
+	default: async ({ cookies, request }) => {
+		let test = await request.formData();
+		console.log(test);
+		let data = {
+			username: 'alice',
+			password: 'pass123'
+		};
+
+		ct_access_token = cookies.get('ct_access_token');
+
+		console.log('ct_access_token from cookie', ct_access_token);
+
+		//data['token'] = cookies.get('token')
+
+		const response = await fetch('http://127.0.0.1:8000/login', {
+			method: 'POST',
+			body: JSON.stringify({ data }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		let response_data = {};
+		await response.json().then((d) => {
+			response_data = d;
+		});
+		console.log(response_data);
+
+		await cookies.set('ct_access_token', response_data.access_token, { path: '/' });
+		cookies.set('ct_refresh_token', response_data.refresh_token, { path: '/' });
+
+		// handle redirect if need
+		if (response.ok) {
+			return redirect(303, '/org/org-austin');
+		}
+	}
+};
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -7,52 +7,54 @@
 		<div class="notification" S>
 			<div class="title has-text-centered">LOGIN</div>
 
-			<div class="field">
-				<label class="label" for="username">Username</label>
-				<div class="control has-icons-left has-icons-right">
-					<input
-						class="input is-success"
-						type="text"
-						id="username"
-						placeholder="Your username"
-						value=""
-					/>
-					<span class="icon is-small is-left">
-						<i class="fa fa-user"></i>
-					</span>
-					<span class="icon is-small is-right">
-						<i class="fa fa-check"></i>
-					</span>
+			<form method="post">
+				<div class="field">
+					<label class="label" for="username">Username</label>
+					<div class="control has-icons-left has-icons-right">
+						<input
+							class="input is-success"
+							type="text"
+							d
+							id="username"
+							name="username"
+							placeholder="Your username"
+							required
+						/>
+						<span class="icon is-small is-left">
+							<i class="fa fa-user"></i>
+						</span>
+						<span class="icon is-small is-right">
+							<i class="fa fa-check"></i>
+						</span>
+					</div>
 				</div>
-			</div>
 
-			<div class="field">
-				<label class="label" for="password">Password</label>
-				<div class="control has-icons-left has-icons-right">
-					<input
-						class="input is-success"
-						type="password"
-						id="password"
-						placeholder="*****"
-						value=""
-					/>
-					<span class="icon is-small is-left">
-						<i class="fas fa-user"></i>
-					</span>
-					<span class="icon is-small is-right">
-						<i class="fas fa-check"></i>
-					</span>
+				<div class="field">
+					<label class="label" for="password">Password</label>
+					<div class="control has-icons-left has-icons-right">
+						<input
+							class="input is-success"
+							type="password"
+							id="password"
+							name="password"
+							placeholder="*****"
+							required
+						/>
+						<span class="icon is-small is-left">
+							<i class="fas fa-lock"></i>
+						</span>
+					</div>
 				</div>
-			</div>
 
-			<div class="field is-grouped">
-				<div class="control">
-					<button class="button is-link" id="login-button">Login</button>
+				<div class="field is-grouped">
+					<div class="control">
+						<button type="submit" class="button is-link" id="login-button">Login</button>
+					</div>
+					<div class="control">
+						<button type="reset" class="button is-light">Cancel</button>
+					</div>
 				</div>
-				<div class="control">
-					<button class="button is-light">Cancel</button>
-				</div>
-			</div>
+			</form>
 
 			<div class="field">
 				<div class="control">
