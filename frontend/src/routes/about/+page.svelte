ddc8e3126a860dcf199dc8177ded9c7097866e80
Regina I. Medina <regimedina19@gmail.com>
2025-05-07 23:04:38 -0500

    Pupulated about page and run linters

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,2 +1,46 @@
-<h1>About Page</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import background_texture from '$lib/assets/background_texture.png';
+	import thread1 from '$lib/assets/illustrations/thread1.png';
+	import thread2 from '$lib/assets/illustrations/thread2.png';
+	import thread3 from '$lib/assets/illustrations/thread3.png';
+</script>
+
+<div class="content is-large">
+	<h1>About</h1>
+	<p>
+		<strong>CommonThread</strong> is an open-source collaborative tool that enables you to turn stories
+		into insights. Community-based organizations, journalists, researchers, and community members alike
+		can use the tool to efficiently gather stories, aggregate them, and understand common threads to
+		draw insights for action.
+	</p>
+	<p>
+		You are the <strong>curator</strong>. You know what issues need to be documented, as well as the
+		people who are the most impacted by them, the
+		<strong>storytellers</strong>. You can now bring all of their stories to a single platform that
+		makes it easier to organize your qualitative data and find relevant patterns. Start threading
+		your story, <a href="signup">sign up </a>.
+	</p>
+</div>
+
+<div class="container is-fluid">
+	<img src={thread2} class="is-centered" alt="Thread illustration 1" />
+</div>
+
+<style>
+	.content {
+		padding: 20px;
+	}
+
+	p {
+		padding: 10px 100px;
+		text-align: center;
+	}
+
+	img {
+		display: flex;
+		justify-content: center;
+		height: 10%;
+		width: 8%;
+		margin: auto;
+	}
+</style>
