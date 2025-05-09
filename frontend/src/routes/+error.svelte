53d385c10d90db7df68b7201e2666864aa026755
Regina I. Medina <regimedina19@gmail.com>
2025-05-09 09:06:59 -0500

    Run linter

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,21 +1,19 @@
 <script>
 	import { page } from '$app/state';
-	import error403 from '$lib/assets/illustrations/error403.png'
-    import error404 from '$lib/assets/illustrations/error404.png'
-    import error500 from '$lib/assets/illustrations/error500.png'
-    import inDevelopment from '$lib/assets/illustrations/error500.png'
+	import error403 from '$lib/assets/illustrations/error403.png';
+	import error404 from '$lib/assets/illustrations/error404.png';
+	import error500 from '$lib/assets/illustrations/error500.png';
+	import inDevelopment from '$lib/assets/illustrations/error500.png';
 </script>
 
-
-<!-- <Error error_img={ error500 }></Error> -->
 <div id="container">
 	<div class="banner">
 		{#if page.status === 500}
-			<img src={ error500 } alt="Customized illustration for HTTP error 500" />
+			<img src={error500} alt="Customized illustration for HTTP error 500" />
 		{:else if page.status === 404}
-			<img src={ error404 } alt="Customized illustration for HTTP error 404" />
+			<img src={error404} alt="Customized illustration for HTTP error 404" />
 		{:else if page.status === 403}
-			<img src={ error403 } alt="Customized illustration for HTTP error 403" />
+			<img src={error403} alt="Customized illustration for HTTP error 403" />
 		{:else}
 			<h1>{page.status}: {page.error.message}</h1>
 		{/if}
