dba5679a640d8b5db20a59be88bb53f61c3ee020
Onurcan Buyukkalkan <onurcan@uchicago.edu>
2025-05-14 14:47:01 -0500

    added tagging process

--- commonthread/ct_application/ml/ml_pipelines/tagging_pipeline.py
+++ commonthread/ct_application/ml/ml_pipelines/tagging_pipeline.py
@@ -1,13 +1,18 @@
 from typing import List
 from ..ml_services.tagging_service import TaggingStrategy
+from transformers import pipeline
 
 
 #example implementation of a tagging strategy
 class MLTaggingStrategy(TaggingStrategy):
     def __init__(self):
-
-        pass
+        ner = pipeline("ner", 
+                       model="dslim/bert-base-NER", 
+                       tokenizer="dslim/bert-base-NER",
+                       aggregation_strategy="simple")
+        self.ner = ner
 
     def get_tags(self, story_text: str) -> List[str]:
-
-        pass
+        ner_results = self.ner(story_text)
+        tags = [result['word'] for result in ner_results]
+        return tags
