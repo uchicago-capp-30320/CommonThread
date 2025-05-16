96e4a9283295c1cfc9b914b5317616822597b326
pravchand <praveenc@uchicago.edu>
2025-05-16 04:01:56 -0500

    small changes to consumer_service notes

--- commonthread/ct_application/cloud/consumer_service.py
+++ commonthread/ct_application/cloud/consumer_service.py
@@ -3,7 +3,6 @@
 This module has 2 main functions:
 1. Keep listening to the sqs queue
 2  Process the message and update the database table (MLProcessingQueue) with the task status.
-This module WILL have to query the DB to get the text contents based on the story_id or project_id in the sqs message.
 """
 from ..ml.ml_services.summarizing_service import SummarizingService
 from ..ml.ml_services.tagging_service import TaggingService
@@ -12,18 +11,7 @@ from ..ml.ml_services.insight_service import InsightService
 from ..models import Story
 from typing import List
 
-#helper functions
-def get_story_text(story_id: int) -> str:
-    story = Story.objects.get(id=story_id)
-    return story.text_content
 
-def get_all_story_text(project_id: int) -> List[str]:
-    stories = Story.objects.filter(proj_id=project_id)
-    return [story.text_content for story in stories]
-
-def get_story_audio(story_id: int) -> str:
-    story = Story.objects.get(id=story_id)
-    return story.audio_file
 
 
 class MLWorkerService:
@@ -36,7 +24,8 @@ class MLWorkerService:
     summary_service = SummarizingService()
     insight_service = InsightService()
     transcribing_service = TranscribingService()
-    
+    transcribing_service.process_story_transcription(story_id) #this pattern to be used for all services
+
 
 
     
