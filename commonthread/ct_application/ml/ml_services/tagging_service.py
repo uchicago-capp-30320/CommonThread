2855946b9307da89cd8eae6095270b5de5ba1a91
pravchand <praveenc@uchicago.edu>
2025-05-16 02:55:45 -0500

    change in interface-service

--- commonthread/ct_application/ml/ml_services/tagging_service.py
+++ commonthread/ct_application/ml/ml_services/tagging_service.py
@@ -1,15 +1,17 @@
-from typing import List, Protocol,Dict
+from typing import List,Dict
 from django.db import transaction
 from ct_application.models import Story, Tag, StoryTag
+from ..ml_pipelines.tagging_pipeline import HFTaggingStrategy, TaggingStrategy
 
-class TaggingStrategy(Protocol):
-    """Protocol defining the interface for tagging implementations"""
-    #gets story text returns a dictionary of tag name,value pairs
-    def get_tags(self, story_text: str) -> List[Dict[str, str]]:
-        pass
+
+
+def get_story_text(story_id: int) -> str:
+    story = Story.objects.get(id=story_id)
+    return story.text_content
+
 
 class TaggingService:
-    def __init__(self, tagging_strategy: TaggingStrategy):
+    def __init__(self, tagging_strategy: TaggingStrategy = HFTaggingStrategy()):
 
         self.tagging_strategy = tagging_strategy
 
@@ -18,9 +20,10 @@ class TaggingService:
         return self.tagging_strategy.get_tags(story_text)
 
     @transaction.atomic
-    def process_story_tags(self, story: Story) -> List[Tag]:
+    def process_story_tags(self, story_id: int) -> bool:
         
-        suggested_tags = self._get_ml_tags_for_story(story.content)
+        story_text = get_story_text(story_id)
+        suggested_tags = self._get_ml_tags_for_story(story_text)
         
         created_tags = []
         
@@ -34,7 +37,7 @@ class TaggingService:
                 created_by = "ML_created"
             )
             
-            
+            story = Story.objects.get(id=story_id)
             StoryTag.objects.get_or_create(
                 story=story,
                 tag=tag
