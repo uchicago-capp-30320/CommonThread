884069c62f1fb04f877afce9d01135f7ce7bbaa8
pravchand <praveenc@uchicago.edu>
2025-05-20 01:53:07 -0500

    black formatting for tagging

--- commonthread/ct_application/ml/ml_pipelines/tagging_pipeline.py
+++ commonthread/ct_application/ml/ml_pipelines/tagging_pipeline.py
@@ -5,27 +5,32 @@ from abc import ABC, abstractmethod
 
 class TaggingStrategy(ABC):
     """Protocol defining the interface for tagging implementations"""
-    #gets story text returns a dictionary of tag name,value pairs
+
+    # gets story text returns a dictionary of tag name,value pairs
     @abstractmethod
     def get_tags(self, story_text: str) -> List[Dict[str, str]]:
         pass
 
-#example implementation of a tagging strategy
+
+# example implementation of a tagging strategy
 class HFTaggingStrategy(TaggingStrategy):
-    def __init__(self, aggregation_strategy: str = "simple", 
-                 model_name: str = "dslim/bert-base-NER",
-                 tokenizer_name: str = "dslim/bert-base-NER"):
-        
-        ner = pipeline("ner", 
-                       model=model_name, 
-                       tokenizer=tokenizer_name,
-                       aggregation_strategy=aggregation_strategy)
-                       
+    def __init__(
+        self,
+        aggregation_strategy: str = "simple",
+        model_name: str = "dslim/bert-base-NER",
+        tokenizer_name: str = "dslim/bert-base-NER",
+    ):
+
+        ner = pipeline(
+            "ner",
+            model=model_name,
+            tokenizer=tokenizer_name,
+            aggregation_strategy=aggregation_strategy,
+        )
+
         self.ner = ner
 
     def get_tags(self, story_text: str) -> List[Dict[str, str]]:
         ner_results = self.ner(story_text)
         tags = [{"word": r["word"], "label": r["entity_group"]} for r in ner_results]
         return tags
-    
-    
