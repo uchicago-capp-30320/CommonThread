9a52917526b0b1bf9e9e6e03d412d659594fa735
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2025-05-23 15:29:12 -0500

    lint

--- frontend/src/lib/components/StoryFullView.svelte
+++ frontend/src/lib/components/StoryFullView.svelte
@@ -2,50 +2,50 @@
 	let { story } = $props();
 </script>
 
-<div class = "card">
-    <header class="card-header">
-        <h2>Curated by <span class="curator">{story.curator}</span> for <span class="project">{story.project_name}</span></h2>
-    </header>
-    <div class="content">
-        <h1>Story by <span class="storyteller"><b>{story.storyteller}</b></span></h1>
-        {#each story.tags as tag}
-            <div class="tags are-medium m-2">
-                <span class="tag">{tag.name}</span>
-            </div>
-        {/each}
+<div class="card">
+	<header class="card-header">
+		<h2>
+			Curated by <span class="curator">{story.curator}</span> for
+			<span class="project">{story.project_name}</span>
+		</h2>
+	</header>
+	<div class="content">
+		<h1>Story by <span class="storyteller"><b>{story.storyteller}</b></span></h1>
+		{#each story.tags as tag}
+			<div class="tags are-medium m-2">
+				<span class="tag">{tag.name}</span>
+			</div>
+		{/each}
 
-        <!-- Row of tags  -->
-        <p>{story.text_content}</p>
-    </div>
+		<!-- Row of tags  -->
+		<p>{story.text_content}</p>
+	</div>
 </div>
 
 <style>
-    .card-header {
-        background-color: #ede8eb;
-        padding: 10px;
-    }
+	.card-header {
+		background-color: #ede8eb;
+		padding: 10px;
+	}
 
-    .content {
-        padding: 25px;
-    }
+	.content {
+		padding: 25px;
+	}
 
-    .tag {
-        background-color: #d0fdb9;
-    }
+	.tag {
+		background-color: #d0fdb9;
+	}
 
-
-    /* 
+	/* 
     span.storyteller {
         color: #56bcb3;
     } */
 
-    /* span.curator {
+	/* span.curator {
 
     }
 
     span.project {
 
     } */
-    
-
 </style>
