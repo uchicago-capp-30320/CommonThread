3b05e2e79a391ea3ec84ebc14b4cad16e21ef395
Regina I. Medina <regimedina19@gmail.com>
2025-05-25 01:35:08 -0500

    Customized footer with branding colors and added icon

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -6,8 +6,12 @@
 	<div class="content has-text-centered">
 		<p>
 			<strong>CommonThread</strong> is an
-			<a href="https://github.com/uchicago-capp-30320/CommonThread"> open source project</a>, part
-			of
+			<a href="https://github.com/uchicago-capp-30320/CommonThread"> open source project
+				<span class="icon">
+					<b><i class="fa fa-code-fork"></i></b>
+				</span>
+			</a>
+			part of
 			<a href="https://github.com/uchicago-capp-30320">
 				Software Engineering for Civic Tech (CAPP 30320)</a
 			>.
@@ -22,4 +26,16 @@
 		width: 100%;
 		padding: 20px;
 	}
+
+	p {
+		color: #133335; 
+	}
+	a {
+		color: #56bcb3;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
 </style>
