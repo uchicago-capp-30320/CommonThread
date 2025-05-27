57c3b9914e7e5464e334da015dd5c599315eb840
Regina I. Medina <regimedina19@gmail.com>
2025-05-26 19:32:43 -0500

    Enable sticky footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -7,8 +7,44 @@
 	let { children } = $props();
 </script>
 
-<Header />
+<!-- 
+Add full page wrapper and article element to enable sticky footer
+Ref: https://stackoverflow.com/questions/12239166/footer-at-bottom-of-page-or-content-whichever-is-lower
+  -->
 
-{@render children()}
+<div id="main-wrapper">
+	<Header />
+	<article>
+		{@render children()}
+	</article>
+	<Footer />
+</div>
 
-<Footer />
+
+<style>
+	:global(html), :global(body) {
+		height: 100%;
+		margin: 0;
+		padding: 0;
+	}
+
+	#main-wrapper {
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-box-orient: vertical;
+	-webkit-box-direction: normal;
+		-ms-flex-direction: column;
+			flex-direction: column;
+	min-height: 100%;
+	height: auto;
+	height: 100%;
+	margin: 0 auto -142px; /* the bottom margin is the negative value of the footer's height */
+	}
+
+	article {
+		-webkit-box-flex: 1;
+		-ms-flex: 1;
+		flex: 1;
+			}
+</style>
